8bd55e1ad0beb693560f592a4ea4e31edbad0a64
anatolyandreev <36275905+anatolyandreev@users.noreply.github.com>
2018-04-13 17:42:32 +0300

    Add files via upload

--- library-records/src/test/java/by/htp/library/example/MailRuSendEmailTest.java
+++ library-records/src/test/java/by/htp/library/example/MailRuSendEmailTest.java
@@ -2,13 +2,13 @@ package by.htp.library.example;
 
 import static org.testng.Assert.assertTrue;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -17,13 +17,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class MailRuSendEmailTest {
+public class MailRuSendEmailTest
+{
 
 	private WebDriver driver;
 	private final static String targetEmail = "qaq959@mail.ru";
@@ -34,17 +34,24 @@ public class MailRuSendEmailTest {
 	private final static String senderPassword = "Klopik123";
 
 	@BeforeMethod
-	public void loginToEmailBox() throws InterruptedException {
-		openPage("http://mail.ru");
+	public void loginToEmailBox() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\AutomationTools\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.DAYS);
 	}
 
 	@AfterMethod
-	public void afterTest() {
+	public void afterTest()
+	{
 		driver.close();
 	}
 
 	@Test
-	public void sendEmailTest() throws InterruptedException {
+	public void sendEmailTest() throws InterruptedException
+	{
+		openPage("http://mail.ru");
 		loginAsUser(senderEmail, senderPassword);
 		sendAnEmail(targetEmail, generateRandomSubject());
 		String expectedValue = driver.findElement(By.xpath("//span[@class='message-sent__info']")).getText();
@@ -52,36 +59,64 @@ public class MailRuSendEmailTest {
 	}
 
 	@Test
-	public void backAndForwardTest() throws InterruptedException {
+	public void backAndForwardTest() throws InterruptedException
+	{
+		openPage("http://mail.ru");
 		loginAsUser(targetLogin, targetPassword);
-		FluentWait<WebDriver> wait = new FluentWait(driver).withTimeout(1, TimeUnit.DAYS).pollingEvery(5, TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
-		ExpectedCondition condition = ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='b-letters']//*[contains(@class, 'item_unread')]"));
-		WebElement elementToWait = (WebElement) wait.until(condition);
-		
-		WebElement firstInbox = driver.findElement(By.xpath("//div[@class='b-datalist__body']/child::*[1]"));
-		if (firstInbox.getAttribute("class").contains(String.valueOf("item_unread"))) {
-			firstInbox.click();
-			WebElement replyButton = driver.findElement(
-					By.xpath("//span[text()='Ответить' and @class='b-toolbar__btn__text b-toolbar__btn__text_pad']"));
-			replyButton.click();
-			driver.switchTo().activeElement().sendKeys(Keys.CONTROL, Keys.RETURN);
-//			Thread.sleep(5000);
-			List<WebElement> list = driver.findElements(By.xpath("//div[@id='MailRuConfirm']//span[1][text()='Продолжить']"));
-			 WebElement continueButton = list.get(1);
-			continueButton.click();
+		while (true)
+		{
+			waitForNewEmailAndOpen();
+			replySimply();
+			navigateToInbox();
 		}
-
 	}
 
-	private String generateRandomSubject() {
+	private void getSenderEmail()
+	{
+		
+	}
+
+	private void navigateToInbox()
+	{
+		driver.findElement(By.xpath("//span[@class='b-nav__item__text b-nav__item__text_unread']")).click();
+		driver.switchTo().defaultContent();
+	}
+
+	private void replySimply() throws InterruptedException
+	{
+		Thread.sleep(1000); //если убрать этот слип будет валиться StaleElementReferenceException
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		ExpectedCondition condition = ExpectedConditions.visibilityOf(
+				driver.findElement(By.xpath("//span[text()='Ответить' and @class='b-toolbar__btn__text b-toolbar__btn__text_pad']")));
+		WebElement replyButton = (WebElement) wait.until(condition);
+		replyButton.click();
+		driver.switchTo().activeElement().sendKeys(Keys.CONTROL, Keys.RETURN);
+		WebElement continueButton = driver.findElement(
+				By.xpath("//span[text()='Продолжить']//parent::*[@class='btn btn_stylish btn_main confirm-ok']"));
+		continueButton.click();
+	}
+
+	private void waitForNewEmailAndOpen()
+	{
+		FluentWait wait = new FluentWait<>(driver).withTimeout(Duration.ofDays(1)).pollingEvery(Duration.ofSeconds(1)).ignoring(
+				NoSuchElementException.class);
+		ExpectedCondition condition = ExpectedConditions.visibilityOf(
+				driver.findElement(By.xpath("//div[@id='b-letters']//*[contains(@class, 'item_unread')]")));
+		WebElement unreadInboxEmail = (WebElement) wait.until(condition);
+		unreadInboxEmail.click();
+	}
+
+	private String generateRandomSubject()
+	{
 		Random rnd = new Random();
 		int n = 100000 + rnd.nextInt(900000);
 		return "Automation test subject" + String.valueOf(n);
 	}
 
-	private void sendAnEmail(String targetEmail, String subject) {
-		WebElement newLetterButton = (new WebDriverWait(driver, 10).until(ExpectedConditions
-				.elementToBeClickable(By.xpath("//*[@data-name='compose' and @title='Написать письмо (N)']"))));
+	private void sendAnEmail(String targetEmail, String subject)
+	{
+		WebElement newLetterButton = (new WebDriverWait(driver, 10).until(
+				ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-name='compose' and @title='Написать письмо (N)']"))));
 		newLetterButton.click();
 		WebElement inputFieldTo = driver.findElement(By.xpath("//textarea[@data-original-name='To']"));
 		inputFieldTo.sendKeys(targetEmail);
@@ -92,7 +127,8 @@ public class MailRuSendEmailTest {
 		driver.findElement(By.xpath("//*[text()='Отправить']")).click();
 	}
 
-	private void loginAsUser(String email, String password) throws InterruptedException {
+	private void loginAsUser(String email, String password) throws InterruptedException
+	{
 		WebElement loginInput = driver.findElement(By.id("mailbox:login"));
 		WebElement passwordInput = driver.findElement(By.id("mailbox:password"));
 		loginInput.sendKeys(email);
@@ -101,17 +137,13 @@ public class MailRuSendEmailTest {
 		Thread.sleep(5000);
 	}
 
-	private void openPage(String url) {
-		System.setProperty("webdriver.chrome.driver", "c:/driver/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.DAYS);
+	private void openPage(String url)
+	{
 		driver.get(url);
 	}
 
-	private void signOut() {
-		// driver.findElement(By.xpath("//*[@data-title='выход']")).click();
-		(new WebDriverWait(driver, 10)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-title='выход']")))).click();
+	private void signOut()
+	{
+		(new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-title='выход']")))).click();
 	}
 }
